430257f22700c67bdbf085a5dd86b993bb7fa37c
LealLara <motionverso0@gmail.com>
2026-05-26 13:44:36 -0300

    melhorias

---
+++ DotNetFlix.Domain/Models/Favorite.cs
@@ -0,0 +1,13 @@
+﻿namespace DotNetFlix.Domain.Models
+{
+    public class Favorite
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public string VideoName { get; set; } = string.Empty;
+
+        public User User { get; set; }
+    }
+}
